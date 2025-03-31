b974540445da1e9efb0d4d3df1d6dc79154ed1c6
Nicolas <131992137+Yeetleer@users.noreply.github.com>
2025-03-31 09:16:06 +0200

    Complétation de AttractionDao

--- src/main/java/DAO/AttractionDao.java
+++ src/main/java/DAO/AttractionDao.java
@@ -55,7 +55,7 @@ public class AttractionDao implements AttractionInterface {
 
         try {
             connection = sqlDatabase.getConnection();
-            preparedStatement = connection.prepareStatement("INSERT INTO Attraction(id, nom) values(?,?)");
+            preparedStatement = connection.prepareStatement("INSERT INTO Attraction(ID, nom) values(?,?)");
             preparedStatement.setInt(1, attraction.getAttractionID());
             preparedStatement.setString(2, attraction.getAttractionName());
             preparedStatement.executeUpdate();
@@ -76,26 +76,93 @@ public class AttractionDao implements AttractionInterface {
 
     @Override
     public void deleteAttraction(Attraction attraction) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
 
+        try {
+            connection = sqlDatabase.getConnection();
+            preparedStatement = connection.prepareStatement("DELETE from Attraction where ID = ?");
+            preparedStatement.setInt(1, attraction.getAttractionID());
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Suppression de Attraction impossible");
+        } finally {
+            try {
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Erreur de fermeture des ressources");
+            }
+        }
     }
 
     @Override
-    public Attraction findAttraction(Attraction attraction) {
-        return null;
+    public Attraction findAttraction(Attraction attractionID) {
+        Attraction attractionFound = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = sqlDatabase.getConnection();
+            preparedStatement = connection.prepareStatement("select * from Attraction where ID = ?");
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                String attractionName = resultSet.getString("nom");
+
+                if (attractionID == attractionID) {
+                    attractionFound = new Attraction(id, attractionName);
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Attraction introuvable");
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Erreur de fermeture des ressources");
+            }
+        }
+        return attractionFound;
     }
 
     @Override
     public Attraction editAttraction(Attraction attraction) {
-        return null;
-    }
 
-    public void deleteUser(User user) { }
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
-    public User findUser(User user) {
-        return null;
-    }
+        try {
+            connection = sqlDatabase.getConnection();
+            preparedStatement = connection.prepareStatement("UPDATE Attraction set ID=?, nom=?");
+            preparedStatement.setInt(1, attraction.getAttractionID());
+            preparedStatement.setString(2, attraction.getAttractionName());
+            preparedStatement.executeUpdate();
 
-    public User editUser(User user) {
-        return null;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erreur de modification de Attraction");
+        } finally {
+            try {
+                if (resultSet != null) resultSet.close();
+                if (preparedStatement != null) preparedStatement.close();
+                if (connection != null) connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Erreur de fermeture des ressources");
+            }
+        }
+        return attraction;
     }
 }
