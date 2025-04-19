f0e0940ae3d828e77319ba19cb70bea52e22b153
lasbo <mathis.henry@edu.ece.fr>
2025-04-19 17:55:43 +0200

    added Session

---
+++ src/main/java/Modele/Session.java
@@ -0,0 +1,29 @@
+package Modele;
+
+public class Session {
+    private static User user;
+
+    public static void setUser(User user) {
+        Session.user = user;
+    }
+
+    public static User getUser() {
+        return user;
+    }
+
+    public static boolean isAdmin() {
+        return user instanceof Administrator;
+    }
+
+    public static void clear() {
+        user = null;
+    }
+
+    public static boolean isLoggedIn() {
+        return user != null;
+    }
+
+    public static boolean isClient() {
+        return user instanceof Client;
+    }
+}
