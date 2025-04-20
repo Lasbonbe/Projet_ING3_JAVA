ef653baeafea92f7dd9a0de89a5f6e71a4385513
lasbo <mathis.henry@edu.ece.fr>
2025-04-20 17:14:14 +0200

    removed date from Invite bc no reduc

--- src/main/java/Modele/Invite.java
+++ src/main/java/Modele/Invite.java
@@ -2,10 +2,8 @@ package Modele;
 import java.sql.Date;
 
 public class Invite extends User {
-    private Date birthdate;
 
-    public Invite(int userID,String firstName, String lastName,String email, Date birthdate) {
+    public Invite(int userID,String firstName, String lastName,String email) {
         super(0,firstName,lastName,email,"");
-        this.birthdate = birthdate;
     }
 }
