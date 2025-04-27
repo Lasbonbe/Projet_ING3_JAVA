2179a7f82ebad1db4da8be34dff73c4de6947ed4
lasbo <mathis.henry@edu.ece.fr>
2025-04-27 20:37:42 +0200

    ScheduleFillerProMax ????

---
+++ src/main/java/DAO/ScheduleFillerProMax.java
@@ -0,0 +1,43 @@
+package DAO;
+
+import Modele.Attraction;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/** Classe poir remplir les Schedules des différentes attractions
+ * @version 1.0
+ */
+public class ScheduleFillerProMax {
+    private static LocalTime startTime = LocalTime.of(9, 0);
+    private static LocalTime endTime = LocalTime.of(17, 0);
+    // 3 mois de réservation en avance
+    private static ScheduleDAO scheduleDAO = new ScheduleDAO();
+
+    /**
+     * Remplit les horaires d'une attraction entre startTime et endTime
+     * La méthode va chercher la durée de l'attraction dans la base de données
+     * puis génère tout les schedules entre startTime et endTime par intervalles de la durée de l'attraction
+     *
+     * @param attraction Une attraction
+     */
+    public static void generateSchedulesForAttraction(Attraction attraction) {
+        int duration = attraction.getDuration();
+        LocalDate currentDate = LocalDate.now();
+        LocalDate endDate = currentDate.plusMonths(1);
+
+
+        while (!currentDate.isAfter(endDate)) {
+            LocalTime currentTime = startTime;
+            while (currentTime.plusMinutes(duration).isBefore(endTime)
+                    || currentTime.plusMinutes(duration).equals(endTime)) {
+                java.sql.Time startTimeSQL = java.sql.Time.valueOf(currentTime);
+                java.sql.Time endTimeSQL = java.sql.Time.valueOf(currentTime.plusMinutes(duration));
+                scheduleDAO.addScheduleWithDuration(attraction, java.sql.Date.valueOf(currentDate), startTimeSQL, endTimeSQL);
+                currentTime = currentTime.plusMinutes(duration);
+            }
+            currentDate = currentDate.plusDays(1);
+        }
+    }
+}
