808d76f80c832911ac17ec570dca6e70c3f4cb46
lasbo <mathis.henry@edu.ece.fr>
2025-04-27 19:18:22 +0200

    renamed

--- src/main/java/Modele/UserNotAdminException.java
+++ src/main/java/Modele/UserNotAdminException.java
@@ -3,8 +3,8 @@ package Modele;
 /**
  * Exeption class for handling client-related errors in a JavaFX application.
  */
-public class ClientNotAdminException extends RuntimeException {
-    public ClientNotAdminException(String message) {
+public class UserNotAdminException extends RuntimeException {
+    public UserNotAdminException(String message) {
         super(message);
     }
 }
