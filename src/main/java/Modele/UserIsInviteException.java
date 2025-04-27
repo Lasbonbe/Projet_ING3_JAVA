8d40e524c5b1a1da7002cf0f137d755314db957d
lasbo <mathis.henry@edu.ece.fr>
2025-04-27 16:35:42 +0200

    javadoc

--- src/main/java/Modele/UserIsInviteException.java
+++ src/main/java/Modele/UserIsInviteException.java
@@ -1,5 +1,11 @@
 package Modele;
 
+/**
+ * Classe d'exception pour les utilisateurs invités.
+ * Hérite de RuntimeException.
+ * Utilisée pour signaler une erreur lorsque l'utilisateur est un invité.
+ * et non un client ou un administrateur, lorsqu'il essaye d'accéder à une fonctionnalité réservée.
+ */
 public class UserIsInviteException extends RuntimeException {
     public UserIsInviteException(String message) {
         super(message);
